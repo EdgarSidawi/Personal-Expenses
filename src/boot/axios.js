0ae0243ee431835c9320bcd479ba5e92a57f39aa
Edgar Sidawi <romantibuai@gmail.com>
2020-01-25 19:16:46 +0000

    created axios instance

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+const axiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:8000'
+})
+
+Vue.prototype.$axios = axiosInstance
+
+export { axiosInstance }
