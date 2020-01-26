7a597f12b168c05c8a61b1d64cb260ba7d915a73
Edgar Sidawi <romantibuai@gmail.com>
2020-01-26 13:46:16 +0000

    add new expense method added

--- src/pages/Expenses.vue
+++ src/pages/Expenses.vue
@@ -23,23 +23,61 @@
 
         <q-item-section>
           <q-item-label>{{ expense.title }}</q-item-label>
-          <q-item-label caption lines="1">{{ expense.date }}</q-item-label>
+          <q-item-label caption lines="1">{{ expense.created_at }}</q-item-label>
         </q-item-section>
 
         <q-item-section side>
-          <q-icon name="edit" color="secondary" @click="Edit" />
+          <q-icon name="edit" color="secondary" @click="Edit(index)" />
         </q-item-section>
         <q-item-section side>
-          <q-icon name="delete" color="red" @click="Delete" />
+          <q-icon name="delete" color="red" @click="Delete(index)" />
         </q-item-section>
         <q-separator />
       </q-item>
     </q-list>
 
-    <q-page-sticky position="bottom" :offset="[18, 18]">
-      <q-btn fab icon="add" color="accent" />
+    <q-page-sticky position="bottom" :offset="[18, 18]" >
+      <q-btn fab icon="add" color="accent" @click="showDialog=true"/>
     </q-page-sticky>
+
+    <q-dialog v-model="showDialog" >
+      <q-card class="my-card q-pa-md" >
+        <q-form
+        @submit.prevent
+        class="q-gutter-md"
+        >
+          <q-input
+            filled
+            v-model="form.title"
+            label="Your title "
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+          />
+
+          <q-input
+            filled
+            type="amount"
+            v-model="form.amount"
+            label="The amount "
+            lazy-rules
+            :rules="[
+              val => val !== null && val !== '' || 'Please type your amount',
+              val => val > 0  || 'Please type a real number'
+            ]"
+          />
+
+          <div>
+            <q-btn label="Add Expense"
+            type="submit"
+            color="primary"
+            @click="newExpense"/>
+          </div>
+        </q-form>
+      </q-card>
+    </q-dialog>
+
   </q-page>
+
 </template>
 
 <script>
@@ -48,6 +86,12 @@ import { mapActions, mapState } from 'vuex'
 export default {
   data () {
     return {
+      editing: false,
+      form: {
+        title: '',
+        amount: null
+      },
+      showDialog: false,
       progress1: 0.3
     }
   },
@@ -59,12 +103,22 @@ export default {
     }
   },
   methods: {
-    ...mapActions('store', ['getExpenses']),
-    Edit () {
+    ...mapActions('store', ['getExpenses', 'addExpense']),
+    Edit (index) {
       console.log(this.expenses)
     },
-    Delete () {
+    Delete (index) {
       alert('Deleting')
+    },
+    resetForm () {
+      this.form = {}
+    },
+    newExpense () {
+      if (this.form.title !== '' && this.form.amount != null) {
+        this.addExpense(this.form)
+        this.resetForm()
+        this.showDialog = false
+      }
     }
   },
   created () {
@@ -72,3 +126,9 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+  .my-card
+    width: 100%
+    max-width: 250px
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { axiosInstance } from '../boot/axios'
 
 const state = {
-  expenses: null
+  expenses: []
 
 }
 
@@ -12,6 +12,9 @@ const getters = {
 const mutations = {
   setExpenses (state, payload) {
     state.expenses = payload
+  },
+  addNewExpense (state, payload) {
+    state.expenses.push(payload)
   }
 
 }
@@ -20,6 +23,11 @@ const actions = {
   getExpenses ({ commit }) {
     axiosInstance.get('api/1/expense').then(res => {
       commit('setExpenses', res.data)
+    })
+  },
+  addExpense ({ commit }, payload) {
+    axiosInstance.post('api/1/expense', payload).then(res => {
+      commit('addNewExpense', { title: res.data.title, amount: res.data.amount })
     })
   }
 }
